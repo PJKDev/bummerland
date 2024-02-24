cee324444fd059e564ae73317d75de0ad61be955
PJKDev <161081073+PJKDev@users.noreply.github.com>
2024-02-23 21:06:37 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,64 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Jack":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f e e f f f . . . . 
+. . . f f f e e e e f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f d e e e e e e d f f . . 
+. . f d d e e f f e e d d f . . 
+. . f f f f f f f f f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f 4 e 4 1 f d d f 1 4 e 4 f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e f f e e e f . . . 
+. . e 4 f 1 1 1 1 1 1 f 4 e . . 
+. . 4 d f 1 1 1 1 1 1 f d 4 . . 
+. . 4 4 f 1 1 1 1 1 1 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "Bummerland":return hex`008c000408030200001c00010a006400f401640000040000000000000000000000000005000004240000000400012a04000800012708000c00012a14001800012418001c0001271c002000012006001c00010a006400f401640000040000000000000000000000000000000002300028002c0001272c003000012430003400012938003c0001253c004000012240004400012744004800012248004c00011e`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
